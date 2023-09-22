0b5599cea96e415fe8edbaaa74fd942b76baa61b
Rakib-spickolab <rakib@spickolab.net>
2023-09-23 01:13:13 +0600

    Update URL

--- src/redux/action/appointmentPageAction.js
+++ src/redux/action/appointmentPageAction.js
@@ -3,37 +3,37 @@ import axios from 'axios'
 
 
 export const fetchAppointPageData = (category,status,doctor,date) => async(dispatch) => {
-    const response = await axios.get(`https://clinic-management-dashboard-backend.vercel.app//clinic/appointment?category=${category}&status=${status}&doctor=${doctor}&date=${date}`)
+    const response = await axios.get(`https://clinic-management-dashboard-backend.vercel.app/clinic/appointment?category=${category}&status=${status}&doctor=${doctor}&date=${date}`)
 
     dispatch({type: actionTypesAppointment.GET_APPOINT_DATA, payload: response.data})
 }  
 
 export const fetchCategoryData = () => async(dispatch) => {
-    const response = await axios.get(`https://clinic-management-dashboard-backend.vercel.app//clinic/appointment/category`)
+    const response = await axios.get(`https://clinic-management-dashboard-backend.vercel.app/clinic/appointment/category`)
 
     dispatch({type: actionTypesAppointment.GET_APPOINT_CATEGORY_DATA, payload: response.data})
 }  
 
 export const fetchDoctorData = () => async(dispatch) => {
-    const response = await axios.get(`https://clinic-management-dashboard-backend.vercel.app//clinic/doctors?accountStatus=active&sorting=desc`)
+    const response = await axios.get(`https://clinic-management-dashboard-backend.vercel.app/clinic/doctors?accountStatus=active&sorting=desc`)
 
     dispatch({type: actionTypesAppointment.GET_APPOINT_DOCTOR_DATA, payload: response.data})
 }  
 
 export const fetchPateintData = (search) => async(dispatch) => {
-    const response = await axios.get(`https://clinic-management-dashboard-backend.vercel.app//clinic/pateints?status=active&searchKey=${search}`)
+    const response = await axios.get(`https://clinic-management-dashboard-backend.vercel.app/clinic/pateints?status=active&searchKey=${search}`)
 
     dispatch({type: actionTypesAppointment.GET_APPOINT_PATEINT_DATA, payload: response.data})
 }  
 
 export const makeDone = (id) => async(dispatch) => {
-    const response = await axios.get(`https://clinic-management-dashboard-backend.vercel.app//clinic/appointment/done?id=${id}`)
+    const response = await axios.get(`https://clinic-management-dashboard-backend.vercel.app/clinic/appointment/done?id=${id}`)
 
     dispatch({type: actionTypesAppointment.MAKE_DONE , payload: response.data})
 }  
 
 export const AddAppointAction = (data) => async(dispatch) => {
-    const response = await axios.post(`https://clinic-management-dashboard-backend.vercel.app//clinic/appointment/add`,data)
+    const response = await axios.post(`https://clinic-management-dashboard-backend.vercel.app/clinic/appointment/add`,data)
 
     dispatch({type: actionTypesAppointment.ADD_APPOINT, payload: response.data})
 
--- src/redux/action/mainPageAction.js
+++ src/redux/action/mainPageAction.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 export const fetchMainPageData = () => async (dispatch) => {
-    const response = await axios.get('https://clinic-management-dashboard-backend.vercel.app//clinic/main')
+    const response = await axios.get('https://clinic-management-dashboard-backend.vercel.app/clinic/main')
 
     dispatch({type:actionTypesMainData.GET_MAIN_DATA, payload: response.data})
 }  
